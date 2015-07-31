012267712ae71a7e26bb92f85ca701cfd7645060
Tobias Kuhn <kuhntobias@gmail.com>
2015-07-31 08:50:59 +0200

    Send changes only once a second

--- client/integrate_ckeditor.js
+++ client/integrate_ckeditor.js
@@ -1,12 +1,22 @@
+var changes = {};
+
 $(function() {
+  var i = 1;
   $(".section,.abstract,.page-header,.bibliography").find("p,h1").not(".keywords").each(function() {
     $(this).attr("contenteditable", "true");
+    var id = "editor" + i++;
+    $(this).attr("id", id);
     editor = CKEDITOR.inline($(this).get(0));
     editor.on('change', function(evt) {
-      var data = evt.editor.getData();
-      console.log('CHANGED TEXT: ' + data);
-      $.post( "/event", { text: data } );
+      changes[id] = evt.editor.getData();
     });
   });
+  setInterval(function () {
+    if (!jQuery.isEmptyObject(changes)) {
+      console.log('CHANGES: ' + changes);
+      $.post( "/event", { 'changes': changes } );
+      changes = {};
+    }
+  }, 1000);
 });
 
